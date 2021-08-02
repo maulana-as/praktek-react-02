38fb88747e1e04ba8d750aeb6dc22614e866f26a
adly.ms@gmail.com <maulanaadly05@gmail.com>
2021-08-02 11:12:10 +0700

    [UPDATE] EDIT PRODUCT

--- src/Components/Dialog/index.js
+++ src/Components/Dialog/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { useFirebase } from '../../Components/FirebaseProvider'
-import PropTypes from 'prop-types'
+import { useFirebase } from "../../Components/FirebaseProvider";
+import PropTypes from "prop-types";
 import {
   Button,
   TextField,
@@ -11,46 +11,50 @@ import {
 } from "@material-ui/core";
 import { withRouter } from "react-router";
 
-const DialogAddProduct = ({history, open, handleClose}) => {
-    console.log(handleClose, '<<<handleClose')
-  const { firestore, user } = useFirebase()  
-  const arvisDoc = firestore.collection(`arvis/${user.uid}/product`)
-   const [ nama, setNama ] = useState(""); 
-   const [ error, setError ] = useState("");
-  const [ isSubmit, setSubmit ] = useState(false)
+const DialogAddProduct = ({ history, open, handleClose }) => {
+  const { firestore, user } = useFirebase();
+  const arvisDoc = firestore.collection(`arvis/${user.uid}/product`);
+  const [nama, setNama] = useState("");
+  const [error, setError] = useState("");
+  const [isSubmit, setSubmit] = useState(false);
   const handleSave = async (e) => {
     e.preventDefault();
-    setSubmit(true)
+    setSubmit(true);
     try {
-      if (!nama) { 
-          throw new Error("Product Name is required")
+      if (!nama) {
+        throw new Error("Product Name is required");
       }
-      const newProduct =  await arvisDoc.add({nama})
-      history.push(`product/edit/${newProduct.id}`)
+      const newProduct = await arvisDoc.add({ nama });
+      history.push(`product/edit/${newProduct.id}`);
     } catch (e) {
       setError(e.message);
     }
-    setSubmit(false)
+    setSubmit(false);
   };
 
   return (
-    <Dialog open={open} onClose={handleClose} disableBackdropClick={isSubmit} disableEscapeKeyDown={isSubmit}>
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      disableBackdropClick={isSubmit}
+      disableEscapeKeyDown={isSubmit}
+    >
       <DialogTitle>Create New Product</DialogTitle>
       <DialogContent dividers>
-          <TextField
-            id="nama"
-            label="Product Name"
-            value={nama}
-            onChange={(e) => { 
-                setError("");
-                setNama(e.target.value)
-            }}
-            helperText={error}
-            error={error ? true : false}
-            fullWidth
-            disabled={isSubmit}
-          />
-          {/* <TextField
+        <TextField
+          id="nama"
+          label="Product Name"
+          value={nama}
+          onChange={(e) => {
+            setError("");
+            setNama(e.target.value);
+          }}
+          helperText={error}
+          error={error ? true : false}
+          fullWidth
+          disabled={isSubmit}
+        />
+        {/* <TextField
             name="price"
             type="number"
             label="Product Price"
@@ -65,7 +69,7 @@ const DialogAddProduct = ({history, open, handleClose}) => {
             helperText={error.price}
             error={error.price ? true : false}
           /> */}
-          {/* <TextField
+        {/* <TextField
             name="quantity"
             type="number"
             label="Quantity Product"
@@ -81,19 +85,21 @@ const DialogAddProduct = ({history, open, handleClose}) => {
             error={error.quantity ? true : false}
           /> */}
       </DialogContent>
-          <DialogActions>
-            <Button  disabled={isSubmit} onClick={handleClose}>Cancel</Button>
-            <Button  color="primary" onClick={handleSave} disabled={isSubmit}>
-              Save
-            </Button>
-          </DialogActions>
+      <DialogActions>
+        <Button disabled={isSubmit} onClick={handleClose}>
+          Cancel
+        </Button>
+        <Button color="primary" onClick={handleSave} disabled={isSubmit}>
+          Save
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 };
 
-DialogAddProduct.propTypes =  { 
- open: PropTypes.bool.isRequired,
- handleClose: PropTypes.func.isRequired 
-}
+DialogAddProduct.propTypes = {
+  open: PropTypes.bool.isRequired,
+  handleClose: PropTypes.func.isRequired,
+};
 
-export default  withRouter(DialogAddProduct);
+export default withRouter(DialogAddProduct);
--- src/Pages/Privates/Products/Edit.js
+++ src/Pages/Privates/Products/Edit.js
@@ -1,102 +1,169 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TextField, Button, Grid, Typography } from "@material-ui/core";
 import { useFirebase } from "../../../Components/FirebaseProvider";
-import { useDocument } from 'react-firebase-hooks/firestore/dist/firestore'
+import { useDocument } from 'react-firebase-hooks/firestore'
+import Loading from '../../../Components/Loading'
+import { useSnackbar } from "notistack";
+
 
 const Edit = ({match}) => {
   const { firestore, user } = useFirebase()
-
-  const producArvis = firestore.doc(`arvis/${user.id}/product/${match.params.productId}`)
-  const [snapshot, loading] = useDocument()
+  const producArvis = firestore.doc(`arvis/${user.uid}/product/${match.params.productId}`)
+  const [snapshot, loading] = useDocument(producArvis)  
+  const [isSubmit, setSubmit] = useState(false);
+  const { enqueueSnackbar } = useSnackbar();
   const [form, setForm] = useState({
     nama: "",
-    sku: "",
     price: 0,
     stock: 0,
     description: ""
   });
 
+  console.log(form, '<<formNama')
   const [error, setError] = useState({
     nama: "",
-    sku: "",
-    price: 0,
-    stock: 0,
+    price: "",
+    stock: "",
     description: ""
   });
+
+
+  const validate = () => { 
+      let newError = { ...error }
+      if (!form.nama) { 
+          newError.nama = 'Product name is required'
+      } 
+      if (!form.price) { 
+          newError.price = 'Product price is required'
+      } else if (form.price <= 0) { 
+          newError.price = 'Product price cannot less than 0'
+      }
+      if (!form.stock) { 
+          newError.stock = 'Product stock is required'
+      } else if (form.stock <= 0) { 
+          newError.stock = 'Product stock cannot less than 0'
+      } 
+
+      if (!form.description) { 
+          newError.description = 'Product description is required'
+      }
+      return newError
+  }
+
+  useEffect(() => { 
+      if (snapshot) { 
+        setForm(currentForm => ({ 
+            ...currentForm,
+            ...snapshot.data()
+        }))
+        console.log(snapshot, '<<<snapshot')
+      }
+  }, [snapshot])
 
   const handleChange = (e) => {
     setForm({
       ...form,
       [e.target.name]: e.target.value,
     });
+    setError({ 
+        ...error,
+        [e.target.name]: ''
+    })
   };
+
+  const handleSubmit = async (e) => { 
+      e.preventDefault();
+      const errors = validate();
+      const findErros = Object.values(errors).some((err) => err !== "");
+      if (findErros) setError(errors);
+      else { 
+          setSubmit(true)
+          try { 
+            await producArvis.set(form, { merge: true })
+            enqueueSnackbar(`Data success be saved on documen`, {
+                variant: "success",
+              });
+          } catch(e) { 
+            enqueueSnackbar(`${e.message}`, {
+                variant: "error",
+              });
+            }
+            setSubmit(false)
+      }
+  }
+
+  if (loading) { 
+    return <Loading />
+  }
 
   return (
     <div>
       <Grid container alignItems="center" justify="center">
         <Grid item xs={12} sm={6}  md={6} lg= {6} >
-          <form>
+          <form id="edit-form" onSubmit={handleSubmit} noValidate>
             <TextField
               id="nama"
+              name="nama"
               label="Product Name"
               margin="normal"
               fullwidth
+              required
               value={form.nama}
               onChange={handleChange}
               helperText={error.nama}
               error={error.nama ? true : false}
-            />
-            <TextField
-              id="sku"
-              label="Product Sku"
-              margin="normal"
-              fullwidth
-              value={form.sku}
-              onChange={handleChange}
-              helperText={error.sku}
-              error={error.sku ? true : false}
+              disabled={isSubmit}
             />
             <TextField
               id="price"
+              name="price"
               label="Product Price"
               margin="normal"
               type="number"
               fullwidth
+              required
               value={form.price}
               onChange={handleChange}
               helperText={error.price}
               error={error.price ? true : false}
+              disabled={isSubmit}
             />
             <TextField
               id="stock"
-              label="Product Price"
+              name="stock"
+              label="Product Stock"
               type="number"
               margin="normal"
               fullwidth
+              required
               value={form.stock}
               onChange={handleChange}
               helperText={error.stock}
               error={error.stock ? true : false}
+              disabled={isSubmit}
             />
             <TextField
               id="description"
+              name="description"
               label="Product Description"
               margin="normal"
               multiline
               rowsMax={3}
               fullwidth
+              required
               value={form.description}
               onChange={handleChange}
               helperText={error.description}
               error={error.description ? true : false}
+              disabled={isSubmit}
             />
           </form>
         </Grid>
         <Grid item xs={12} sm={6} md={6}>
             <Typography>Upload Image</Typography>
         </Grid>
-        <Grid item xs={12}>
-            <Button color="primay" variant="contained">Save</Button>
+        <Grid item xs={12} style={{marginTop: '10px'}}>
+            <Button form="edit-form" type="submit" color="primary" variant="contained">Save</Button>
         </Grid>
       </Grid>
     </div>
