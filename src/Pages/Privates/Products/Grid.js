c919866b92836bd71a663089e8859fdc981baee6
adly.ms@gmail.com <maulanaadly05@gmail.com>
2021-08-02 20:26:08 +0700

    [UPDATE] Lit Product

--- src/Pages/Privates/Products/Grid.js
+++ src/Pages/Privates/Products/Grid.js
@@ -1,23 +1,85 @@
-import React, { useState } from "react";
-import { Fab } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import {
+  Fab,
+  Card,
+  Grid,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+} from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import useStyles from "./style";
-import DialogAddProduct from '../../../Components/Dialog'
+import DialogAddProduct from "../../../Components/Dialog";
+import { useFirebase } from "../../../Components/FirebaseProvider";
+import { useCollection } from "react-firebase-hooks/firestore";
+import Loading from "../../../Components/Loading";
 
-const Grid = () => {
-  const classes = useStyles()
-  const [openDialog, setOpenDialog] = useState(false)
+const GridProduct = () => {
+  const classes = useStyles();
+  const { firestore, user } = useFirebase();
+
+  const productArvis = firestore.collection(`arvis/${user.uid}/product`);
+  const [snapshot, loading] = useCollection(productArvis);
+  const [productItems, setProductItems] = useState([]);
+  const [openDialog, setOpenDialog] = useState(false);
+
+  useEffect(() => {
+    if (snapshot) {
+      setProductItems(snapshot.docs);
+    }
+  }, [snapshot]);
+
+  if (loading) return <Loading />;
   return (
     <>
-    <Fab className={classes.fab} color="primary" onClick={(e) => { 
-        setOpenDialog(true)
-    }}>
-      <AddIcon />
-    </Fab>
-      <DialogAddProduct open={openDialog} handleClose={() => { 
-          setOpenDialog(false)
-      }}  />
-      </>
+      <Typography variant="h5" component="h1" style={{marginBottom: "20px" }}>
+        Daftar Product :
+      </Typography>{" "}
+      {productItems.length < 0 && <Typography>Product Not Found</Typography>}
+      <Grid container spacing={3}>
+        {
+            // JSON.stringify(productItems)
+            productItems.map(itemProduct => {
+                const product = itemProduct.data()
+                return <Grid key={product.id} item={true} xs={12} sm={12} md={6} lg={4}>
+                <Card>
+                    {
+                     product.photo && 
+                     <CardMedia className={classes.photo}
+                         image={product.photo}
+                         title={product.nama}
+                     /> 
+                    }
+                    <CardContent>
+                        <Typography
+                         variant="h5"
+                         noWrap
+                        >
+                            { product.nama}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+            })
+        }
+      </Grid>
+      <Fab
+        className={classes.fab}
+        color="primary"
+        onClick={(e) => {
+          setOpenDialog(true);
+        }}
+      >
+        <AddIcon />
+      </Fab>
+      <DialogAddProduct
+        open={openDialog}
+        handleClose={() => {
+          setOpenDialog(false);
+        }}
+      />
+    </>
   );
 };
-export default Grid;
+export default GridProduct;
--- src/Pages/Privates/Products/style.js
+++ src/Pages/Privates/Products/style.js
@@ -1,26 +1,29 @@
-import { makeStyles } from '@material-ui/core'
+import { makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles(theme => ({ 
-    fab: { 
-        position: 'absolute',
-        bottom: theme.spacing(2),
-        right: theme.spacing(2)
-    }, 
-    inputImage: { 
-        display: 'none'
-    }, 
-    errorNotifUpload: { 
-        color: 'red',
-        marginTop: theme.spacing(1),
-    },
-    photoProductDisplay: { 
-        textAlign: 'center',
-        padding: theme.spacing(3)
-    },
-    previewProduct: { 
-        width: '100%',
-        height: 'auto',
-    }
-}))
+const useStyles = makeStyles((theme) => ({
+  fab: {
+    position: "absolute",
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+  inputImage: {
+    display: "none",
+  },
+  errorNotifUpload: {
+    color: "red",
+    marginTop: theme.spacing(1),
+  },
+  photoProductDisplay: {
+    textAlign: "center",
+    padding: theme.spacing(3),
+  },
+  previewProduct: {
+    width: "100%",
+    height: "auto",
+  },
+  photo: {
+    width: "150px",
+  },
+}));
 
-export default useStyles
+export default useStyles;
